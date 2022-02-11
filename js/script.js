6d6607a6d36590de134914c27b1dbad0b79c91b0
Dwinanda <dwinanda.wicaksana@student.umn.ac.id>
2022-02-12 01:22:35 +0700

    Fix beberapa bug lagi hue~~~~

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-let searchForm = document.querySelector('.search-form');
-
+// let searchForm = document.querySelector('.search-form');
+//
 // document.querySelector('#search-btn').onclick = () =>{
 //     searchForm.classList.toggle('active');
 //     shoppingCart.classList.remove('active');
@@ -41,26 +41,26 @@ window.onscroll = () =>{
     navbar.classList.remove('active');
 }
 
-// var swiper = new Swiper(".product-slider", {
-//     loop:true,
-//     spaceBetween: 20,
-//     autoplay: {
-//         delay: 7500,
-//         disableOnInteraction: false,
-//     },
-//     centeredSlides: true,
-//     breakpoints: {
-//       0: {
-//         slidesPerView: 1,
-//       },
-//       768: {
-//         slidesPerView: 2,
-//       },
-//       1020: {
-//         slidesPerView: 3,
-//       },
-//     },
-// });
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    centeredSlides: false,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      // 1020: {
+      //   slidesPerView: 3,
+      // },
+    },
+});
 //
 // var swiper = new Swiper(".review-slider", {
 //     loop:true,
