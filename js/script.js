1c01f30c118fdfb4d526431f5b76b12bc6f0c7ed
irene <irene.joseph@student.umn.ac.id>
2022-03-26 18:17:28 +0700

    rapihin js raw

--- js/script.js
+++ js/script.js
@@ -18,48 +18,42 @@
 
 let loginForm = document.querySelector('.login-form');
 
-document.querySelector('#login-btn').onclick = () =>{
-    loginForm.classList.toggle('active');
-    searchForm.classList.remove('active');
-    shoppingCart.classList.remove('active');
-    navbar.classList.remove('active');
+document.querySelector('#login-btn').onclick = () => {
+  loginForm.classList.toggle('active');
+  navbar.classList.remove('active');
 }
 
 let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    shoppingCart.classList.remove('active');
-    loginForm.classList.remove('active');
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.toggle('active');
+  loginForm.classList.remove('active');
 }
 
-window.onscroll = () =>{
-    searchForm.classList.remove('active');
-    shoppingCart.classList.remove('active');
-    loginForm.classList.remove('active');
-    navbar.classList.remove('active');
+window.onscroll = () => {
+  loginForm.classList.remove('active');
+  navbar.classList.remove('active');
 }
 
 var swiper = new Swiper(".product-slider", {
-    loop:true,
-    spaceBetween: 20,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  centeredSlides: false,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    centeredSlides: false,
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      // 1020: {
-      //   slidesPerView: 3,
-      // },
+    768: {
+      slidesPerView: 2,
     },
+    // 1020: {
+    //   slidesPerView: 3,
+    // },
+  },
 });
 //
 // var swiper = new Swiper(".review-slider", {
