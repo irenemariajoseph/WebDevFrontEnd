667605d5b584544e2da248845ec1b22ece508927
irene <irene.joseph@student.umn.ac.id>
2022-03-27 01:13:00 +0700

    benerin timeline dan rapi

--- js/script.js
+++ js/script.js
@@ -1,78 +1,43 @@
-// let searchForm = document.querySelector('.search-form');
-//
-// document.querySelector('#search-btn').onclick = () =>{
-//     searchForm.classList.toggle('active');
-//     shoppingCart.classList.remove('active');
-//     loginForm.classList.remove('active');
-//     navbar.classList.remove('active');
-// }
-//
-// let shoppingCart = document.querySelector('.shopping-cart');
-//
-// document.querySelector('#cart-btn').onclick = () =>{
-//     shoppingCart.classList.toggle('active');
-//     searchForm.classList.remove('active');
-//     loginForm.classList.remove('active');
-//     navbar.classList.remove('active');
-// }
+window.onload = function () {
+  // your code 
 
-let loginForm = document.querySelector('.login-form');
+  let loginForm = document.querySelector('.login-form');
 
-document.querySelector('#login-btn').onclick = () => {
-  loginForm.classList.toggle('active');
-  navbar.classList.remove('active');
-}
+  document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+  }
 
-let navbar = document.querySelector('.navbar');
+  let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () => {
-  navbar.classList.toggle('active');
-  loginForm.classList.remove('active');
-}
+  document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+  }
 
-window.onscroll = () => {
-  loginForm.classList.remove('active');
-  navbar.classList.remove('active');
-}
+  window.onscroll = () => {
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+  }
 
-var swiper = new Swiper(".product-slider", {
-  loop: true,
-  spaceBetween: 20,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  centeredSlides: false,
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
+  var swiper = new Swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
     },
-    768: {
-      slidesPerView: 2,
+    centeredSlides: false,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      // 1020: {
+      //   slidesPerView: 3,
+      // },
     },
-    // 1020: {
-    //   slidesPerView: 3,
-    // },
-  },
-});
-//
-// var swiper = new Swiper(".review-slider", {
-//     loop:true,
-//     spaceBetween: 20,
-//     autoplay: {
-//         delay: 7500,
-//         disableOnInteraction: false,
-//     },
-//     centeredSlides: true,
-//     breakpoints: {
-//       0: {
-//         slidesPerView: 1,
-//       },
-//       768: {
-//         slidesPerView: 2,
-//       },
-//       1020: {
-//         slidesPerView: 3,
-//       },
-//     },
-// });
+  });
+};
