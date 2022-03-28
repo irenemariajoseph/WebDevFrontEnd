2d9b7c803a7c42401d4886ad876e3e796ef03287
irene <irene.joseph@student.umn.ac.id>
2022-03-29 00:00:31 +0700

    bannerrrr

--- js/banner-animation.js
+++ js/banner-animation.js
@@ -9,11 +9,11 @@ function showSlides1() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  if (slideIndex > slides.length) { slideIndex = 1 }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides1, 5000); // Change image every 2 seconds
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides1, 6000); // Change image every 2 seconds
 }
